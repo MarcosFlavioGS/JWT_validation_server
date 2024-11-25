7b140bbb628eaf04e9c921c8e63d96b32fc219db
Marcos Flavio <mfghost69@gmail.com>
2024-11-24 23:43:56 -0300

    feat: add more matches for validation errors

--- lib/stream_chat_web/controllers/auth/fallback_controller.ex
+++ lib/stream_chat_web/controllers/auth/fallback_controller.ex
@@ -3,17 +3,17 @@ defmodule StreamChatWeb.Auth.FallbackController do
 
   use StreamChatWeb, :controller
 
-  def call(conn, {:error, reason}) do
+  def call(conn, {:error, :signature_error}) do
     conn
     |> put_status(:bad_request)
     |> put_view(json: StreamChatWeb.Auth.ErrorJSON)
-    |> render(:error, reason: reason)
+    |> render(:error, reason: :signature_error)
   end
 
-  def call(conn, {:message, reason}) do
+  def call(conn, {:error, _reason}) do
     conn
     |> put_status(:bad_request)
     |> put_view(json: StreamChatWeb.Auth.ErrorJSON)
-    |> render(:error, reason: reason)
+    |> render(:error, reason: "Invalid token")
   end
 end
